c906405945f2f1bd99dc770fe094207ecd29b664
pycharm <pycharm@pycharm>
2021-11-18 02:27:29 +1000

    fix SQL and manage subscr check

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class Database:
     def __init__(self, db_file):
         self.connection = sqlite3.connect(db_file)
@@ -7,10 +8,10 @@ class Database:
 
     def add_user(self, user_id):
         with self.connection:
-           return self.cursor.execute("INSERT INTO 'users' ('user_chat_id') VALUES (?)", (user_id,))
+            return self.cursor.execute("INSERT INTO users (user_chat_id) VALUES (?)", (user_id,))
 
     def user_exists(self, user_id):
         with self.connection:
-            result = self.cursor.execute("SELECT * FROM 'users' WHERE 'user_chat_id' = ?", (user_id,)).fetchall()
+            result = self.cursor.execute("SELECT * FROM users WHERE user_chat_id = ?", (user_id,)).fetchall()
             return bool(len(result))
     
--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from aiogram import Bot, Dispatcher, executor, types
 from db import Database
 
 TOKEN = "2138280761:AAFm9Mo__bmS9lLDBamjyLY4XEdZCHQHyHA"
+CHANNEL_ID = '-1001618707300'
 
 bot = Bot(token=TOKEN)
 db = Database('identifier.sqlite')
 dp = Dispatcher(bot)
+
+
+def check_sub_channel(chat_member):
+    if chat_member['status'] != 'left':
+        return True
 
 
 @dp.message_handler(commands=['start'])
@@ -13,8 +19,10 @@ async def start(message: types.Message):
     if not db.user_exists(message.from_user.id):
         db.add_user(message.from_user.id)
         await bot.send_message(message.from_user.id, 'Укажите имя вашей компании')
+    elif not check_sub_channel(await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=message.from_user.id)):
+        await bot.send_message(message.from_user.id, 'Подпишитесь на канал!')
     else:
-        await bot.send_message(message.from_user.id, 'Вы уже зарегистрированы!')
+        await bot.send_message(message.from_user.id, 'Вы уже зарегистрированы и подписаны!')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
